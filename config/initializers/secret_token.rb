9d7790185645082ef07f52cea6b7d9ba58fc5377
Cifer-Y <1515351520@qq.com>
2014-07-20 20:03:37 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rabel::Application.config.secret_token = '154405d876223fe2cfda163f7e61554d1e122b45fc42990b399f073776aa2424021ae28fd5de0332ce4ad8aac117662a14b802f6719c1eadff92753a2a62a4a8'
